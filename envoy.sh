ee0776aaafd19e5094b66cc0ff6ea3a363da01a4
He Jie Xu <hejie.xu@intel.com>
2021-03-24 01:41:26 +0000

    update

--- envoy.sh
+++ envoy.sh
@@ -6,7 +6,7 @@ ENVOY_CPU_SET=${ENVOY_CPU_SET:=13-17}
 ENVOY_CONCURRENCY=${ENVOY_CONCURRENCY:=4}
 ENVOY_BIN=${ENVOY_BIN:=/home/hejiexu/go/src/github.com/envoyproxy/envoy/bazel-bin//source/exe/envoy-static}
 ENVOY_CONFIG=${ENVOY_CONFIG:=}
-mkidr -p $BASE_DIR
+mkdir -p $BASE_DIR
 touch ${BASE_DIR}/envoy.log
 taskset -c ${ENVOY_CPU_SET} ${ENVOY_BIN} --config-path $ENVOY_CONFIG --concurrency $ENVOY_CONCURRENCY > ${BASE_DIR}/envoy.log 2>&1 &
 echo "envoy started"
