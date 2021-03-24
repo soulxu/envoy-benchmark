9599f6b62310e29c07daede4bb024af320acc118
He Jie Xu <hejie.xu@intel.com>
2021-03-24 01:47:49 +0000

    update

--- benchmark-cpu-affinity.sh
+++ benchmark-cpu-affinity.sh
@@ -46,7 +46,7 @@ export BASE_DIR=$RESULT_DIR/envoy-with-cpu-affinity-with-tls
 export TRANSPORT_OPT="--transport-socket $TRANSPORT_SOCKET"
 echo "Begin to test cpu affinity with tls"
 bash ./benchmark-envoy.sh
-
+sleep 10
 
 # test with cpu affinity but without tls
 export ENVOY_CONFIG=./envoy-http.yaml
@@ -54,7 +54,7 @@ export BASE_DIR=$RESULT_DIR/envoy-with-cpu-affinity-without-tls
 export TRANSPORT_OPT=
 echo "Begin to test cpu affinity without tls"
 bash ./benchmark-envoy.sh
-
+sleep 10
 
 # switch envoy branch to main
 #pushd /home/hejiexu/go/src/github.com/envoyproxy/envoy
@@ -73,6 +73,7 @@ export BASE_DIR=$RESULT_DIR/envoy-without-cpu-affinity-with-tls
 export TRANSPORT_OPT="--transport-socket $TRANSPORT_SOCKET"
 echo "Begin to test without cpu affinity with tls"
 bash ./benchmark-envoy.sh
+sleep 10
 
 # test without cpu affinity and without tls
 export ENVOY_CONFIG=./envoy-http.yaml
--- envoy.sh
+++ envoy.sh
@@ -7,8 +7,7 @@ ENVOY_CONCURRENCY=${ENVOY_CONCURRENCY:=4}
 ENVOY_BIN=${ENVOY_BIN:=/home/hejiexu/go/src/github.com/envoyproxy/envoy/bazel-bin//source/exe/envoy-static}
 ENVOY_CONFIG=${ENVOY_CONFIG:=}
 mkdir -p $BASE_DIR
-touch ${BASE_DIR}/envoy.log
-taskset -c ${ENVOY_CPU_SET} ${ENVOY_BIN} --config-path $ENVOY_CONFIG --concurrency $ENVOY_CONCURRENCY > ${BASE_DIR}/envoy.log 2>&1 &
+taskset -c ${ENVOY_CPU_SET} ${ENVOY_BIN} --config-path $ENVOY_CONFIG --concurrency $ENVOY_CONCURRENCY > ./envoy.log 2>&1 &
 echo "envoy started"
 
 echo "$!" > envoy.pid
