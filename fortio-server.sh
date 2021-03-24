2d59ab13bf2b4b3e58bd24bc5851bc4ac7d9a58c
He Jie Xu <hejie.xu@intel.com>
2021-03-24 01:57:25 +0000

    update

--- fortio-server.sh
+++ fortio-server.sh
@@ -3,6 +3,6 @@
 set -e
 FORTIO_CPU_SET=${FORTIO_CPU_SET:=50-59}
 
-taskset -c ${FORTIO_CPU_SET} fortio server > /dev/null 2>&1 &
+taskset -c ${FORTIO_CPU_SET} fortio server > ./fortio.log 2>&1 &
 
 echo "$!" > ./fortio.pid
