e98abc2aee68c6bbe11b48879a222c71d111b520
He Jie Xu <hejie.xu@intel.com>
2023-07-15 04:19:41 +0000

    update

--- running_benchmark_suite_all_bins_rps_iouring.sh
+++ running_benchmark_suite_all_bins_rps_iouring.sh
@@ -12,7 +12,7 @@ export BACK_SERVER="fortio"
 export LOAD_TARGET="http://127.0.0.1:13333"
 export LOAD_REQUEST_BODY_SIZE=4096
 export LOAD_CONNECTIONS=32
-export LOAD_DURATION=10
+export LOAD_DURATION=60
 
 export LOAD_CPU_SET=3-9
 export BACKEND_CPU_SET=10-22
@@ -24,13 +24,16 @@ export ENVOY_CONFIG=./envoy-iouring.yaml
 export ENVOY_CONCURRENCY=1
 export BACKEND_SERVER_PORT=13334
 
+echo "" > $SUITE_DIR/test.result
+
 for bin in `ls $BIN_DIR`; do
     export ENVOY_BIN=$BIN_DIR/$bin
-    for rps in `seq 1000 500 8000`; do
+    for rps in `seq 1000 200 4000`; do
         export BASE_DIR=$RESULT_BASE_DIR/$bin/$rps
         export LOAD_RPS=${rps}
         echo "Begin to test ${BASE_DIR}"
         bash ./benchmark-envoy.sh
+        echo "$rps `cat ${BASE_DIR}/nighthawk_result.result |grep "min:"| awk -F '|' 'NR == 3 {print $2}'`" >> $SUITE_DIR/test.result
     done
     sleep 3
 done
