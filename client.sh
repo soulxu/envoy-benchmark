5a8099b16812109b051a66cb8f50c8a6ffea786b
He Jie Xu <hejie.xu@intel.com>
2023-07-15 04:29:51 +0000

    update

--- client.sh
+++
@@ -1,5 +0,0 @@
-taskset -c 40-43 fortio load -c 32 -qps 400000 -data-dir . -json fortio_with_envoy_qps_400000_conn_32.json -t 20s http://localhost:13333/
-
-
-
-#-payload-size 100
--- running_benchmark_suite_all_bins_rps_iouring.sh
+++ running_benchmark_suite_all_bins_rps_iouring.sh
@@ -2,8 +2,6 @@ set -e
 
 # All test result is under $SUITE_DIR/result
 export SUITE_DIR=./iouring-rps
-RESULT_BASE_DIR=$SUITE_DIR/result
-mkdir -p $RESULT_BASE_DIR
 echo $SUITE_DIR
 
 export BIN_DIR="./bin"
@@ -18,19 +16,16 @@ export LOAD_CPU_SET=3-9
 export BACKEND_CPU_SET=10-22
 export ENVOY_CPU_SET=1
 
-export TEST_TIMES=1
-
 export ENVOY_CONFIG=./envoy-iouring.yaml
 export ENVOY_CONCURRENCY=1
 export BACKEND_SERVER_PORT=13334
 
-
-
 for bin in `ls $BIN_DIR`; do
     export ENVOY_BIN=$BIN_DIR/$bin
+    mkdir -p $SUITE_DIR/${bin}
     echo -n "" > $SUITE_DIR/${bin}/test.result
     for rps in `seq 1000 200 4000`; do
-        export BASE_DIR=$RESULT_BASE_DIR/$bin/$rps
+        export BASE_DIR=$SUITE_DIR/$bin/$rps
         export LOAD_RPS=${rps}
         echo "Begin to test ${BASE_DIR}"
         bash ./benchmark-envoy.sh
