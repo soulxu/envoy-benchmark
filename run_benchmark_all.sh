fefaa52d30e9e3be8796bdfc89bde7fab75b437a
He Jie Xu <hejie.xu@intel.com>
2023-07-26 00:38:34 +0000

    update

--- run_benchmark_all.sh
+++ run_benchmark_all.sh
@@ -1,10 +1,10 @@
 # load fortio backend ng
 bash ./running_benchmark_suite_all_iouring-load-fortio-back-nighthawk.sh
 # load nighthawk backend ng
-bash ./running_benchmark_suite_all_bins_rps_iouring_load-nighthawk-back-nighthawk.sh
+bash ./running_benchmark_suite_all_iouring-load-nighthawk-back-nighthawk.sh
 
 # load fortio beckend fortio
-bash ./running_benchmark_suite_all_bins_rps_iouring-load-fortio-back-fortio.sh
+bash ./running_benchmark_suite_all_iouring-load-fortio-back-fortio.sh
 
 # load ng backend fortio
-bash ./running_benchmark_suite_all_bins_rps_iouring-load-nighthawk-back-fortio.sh
+bash ./running_benchmark_suite_all_iouring-load-nighthawk-back-fortio.sh
