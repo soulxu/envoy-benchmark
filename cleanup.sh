f5f49b730c3baf70ef61774f34971ae44c5e7807
He Jie Xu <hejie.xu@intel.com>
2023-09-07 02:48:12 +0000

    update cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -6,7 +6,8 @@ BACK_SERVER=${BACK_SERVER:=fortio}
 echo "Kill envoy on $ENVOY_HOST"
 #ssh -i $SSH_KEY hejiexu@$ENVOY_HOST 'bash -c "cd /home/hejiexu/cpu-affinity-benchmark; kill -9 \`cat ./envoy.pid\`"'
 #ssh -i $SSH_KEY hejiexu@$ENVOY_HOST 'bash -c "cd /home/hejiexu/cpu-affinity-benchmark; kill -9 \`pgrep envoy\`"'
-kill -SIGINT `pgrep envoy`
+#kill -SIGINT `pgrep envoy`
+kill -SIGINT `cat ./envoy.pid`
 if [ $PERF_ENABLED = 1 ]; then
     if [ $PERF_TOOL = perf ]; then
         sleep 3
@@ -18,8 +19,10 @@ echo "Kill backserver $BACK_SERVER on $FORTIO_HOST"
 #ssh -i $SSH_KEY hejiexu@$FORTIO_HOST 'bash -c "cd /home/hejiexu/cpu-affinity-benchmark; kill -9 \`pgrep fortio\`"'
 if [ $BACK_SERVER = "nighthawk" ]; then
 #kill -9 `pgrep nighthawk_test_server`
-kill -9 `ps -ax|grep nighthawk_test_server`
+#kill -9 `ps -ax|grep nighthawk_test_server`
+kill -9 `cat nighthawk_test_server.pid`
 elif [ $BACK_SERVER = "fortio" ]; then
-kill -9 `pgrep fortio`
+#kill -9 `pgrep fortio`
+kill -9 `cat fortio.pid`
 fi
 
