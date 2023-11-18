7ba659d9fa24229a04973bf2ff2e20659f7cdd5e
He Jie Xu <hejie.xu@intel.com>
2023-11-18 01:13:13 +0000

    update

--- run_benchmark_all.sh
+++ run_benchmark_all.sh
@@ -1,10 +1,47 @@
-# load fortio backend ng
-bash ./running_benchmark_suite_all_iouring-load-fortio-back-nighthawk.sh
 # load nighthawk backend ng
-bash ./running_benchmark_suite_all_iouring-load-nighthawk-back-nighthawk.sh
 
-# load fortio beckend fortio
-bash ./running_benchmark_suite_all_iouring-load-fortio-back-fortio.sh
+# export TIMES=1
+# export ENVOY_CPU_SET=32
 
+# for conns in `seq 32 8 32`; do
+#   export SUITE_DIR="1times-without-ht-poll-nosubmit/${conns}/iouring-load-nighthawk-back-nighthawk"
+#   export LOAD_CONNECTIONS=${conns}
+#   bash ./running_benchmark_suite_all_iouring-load-nighthawk-back-nighthawk.sh
+
+# done
+export PERF_ENABLED=1
+export PERF_TOOL=perf
+
+export SUITE_DIR=./tls-test/http3
+export LOAD_TARGET=https://www.example.com:13333/
+export ENVOY_CONFIG=./envoy-http3.yaml
+export LOAD_TRANSPORT_OPT="--protocol http3"
+export LOAD_MAX_REQUEST_PER_CONNECTION=1
 # load ng backend fortio
-bash ./running_benchmark_suite_all_iouring-load-nighthawk-back-fortio.sh
+bash ./running_benchmark_suite_all_load-nighthawk-back-fortio.sh
+
+export SUITE_DIR=./tls-test/http3-cmb
+export LOAD_TARGET=https://www.example.com:13333/
+export ENVOY_CONFIG=./envoy-http3-cmb.yaml
+export LOAD_TRANSPORT_OPT="--protocol http3"
+export LOAD_MAX_REQUEST_PER_CONNECTION=1
+# load ng backend fortio
+bash ./running_benchmark_suite_all_load-nighthawk-back-fortio.sh
+
+export SUITE_DIR=./tls-test/http3-qat
+export LOAD_TARGET=https://www.example.com:13333/
+export ENVOY_CONFIG=./envoy-http3-qat.yaml
+export LOAD_TRANSPORT_OPT="--protocol http3"
+export LOAD_MAX_REQUEST_PER_CONNECTION=1
+# load ng backend fortio
+bash ./running_benchmark_suite_all_load-nighthawk-back-fortio.sh
+
+
+
+# # load fortio backend ng
+# bash ./running_benchmark_suite_all_iouring-load-fortio-back-nighthawk.sh
+
+
+# # load fortio beckend fortio
+# bash ./running_benchmark_suite_all_iouring-load-fortio-back-fortio.sh
+
---
+++ running_benchmark_suite_all_load-nighthawk-back-fortio.sh
@@ -0,0 +1,51 @@
+set -e
+
+# All test result is under $SUITE_DIR/result
+export SUITE_DIR=${SUITE_DIR:=./1times-with-ht/iouring-load-nighthawk-back-fortio}
+#export SUITE_DIR=./improve/iouring-load-nighthawk-back-fortio
+echo $SUITE_DIR
+
+export BIN_DIR="./bin"
+export LOAD_CLIENT="nighthawk"
+export BACK_SERVER="fortio"
+export LOAD_TARGET=${LOAD_TARGET:="http://127.0.0.1:13333/?size=4096:100"}
+export LOAD_REQUEST_BODY_SIZE=0
+export LOAD_CONNECTIONS=2000
+export LOAD_DURATION=60
+export LOAD_MODE=closed
+export LOAD_RPS=100
+
+export LOAD_CPU_SET=0-55,112-167
+export BACKEND_CPU_SET=168-223
+export ENVOY_CPU_SET=56
+
+export ENVOY_CONFIG=${ENVOY_CONFIG:=./envoy-iouring.yaml}
+export ENVOY_CONCURRENCY=1
+export BACKEND_SERVER_PORT=13334
+
+export TIMES=1
+export PERF_ENABLED=${PERF_ENABLED:=0}
+
+for bin in `ls $BIN_DIR`; do
+    export ENVOY_BIN=$BIN_DIR/$bin
+    mkdir -p $SUITE_DIR/${bin}
+    echo -n "" > $SUITE_DIR/${bin}/test.result
+    echo -n "" > $SUITE_DIR/${bin}/test_99.result
+    echo -n "" > $SUITE_DIR/${bin}/test_999.result
+    echo -n "" > $SUITE_DIR/${bin}/test_throughput.result
+    for concurrency in 15; do
+        for i in `seq 1 1 $TIMES`; do
+            export BASE_DIR=$SUITE_DIR/$bin/$concurrency/$i
+            export LOAD_CONCURRENCY=${concurrency}
+            echo "Begin to test ${BASE_DIR}"
+            bash ./benchmark-envoy.sh
+            # catch the mean latency of "Request start to response end"
+            echo "$concurrency $i `cat ${BASE_DIR}/nighthawk_result.result |grep "min:"| awk -F '|' 'NR == 3 {print $2}'`" >> $SUITE_DIR/$bin/test.result
+            echo "$concurrency $i `cat ${BASE_DIR}/nighthawk_result.result |grep "0.990"| awk 'NR == 3 {print $3 $4 $5}'`" >> $SUITE_DIR/$bin/test_99.result
+            echo "$concurrency $i `cat ${BASE_DIR}/nighthawk_result.result |grep "0.999"| awk 'NR == 3 {print $3 $4 $5}'`" >> $SUITE_DIR/$bin/test_999.result
+            echo "$concurrency $i `cat ${BASE_DIR}/nighthawk_result.result |grep "benchmark.http_2xx"`" >> $SUITE_DIR/$bin/test_throughput.result
+        done
+    done
+    sleep 3
+done
+
